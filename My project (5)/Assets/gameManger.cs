94a69cb79200dc06ee44d8be7ca34c80ded47929
Faisal4884 <109870272+Faisal4884@users.noreply.github.com>
2022-08-10 18:22:40 -0500

    CW5

--- My project (5)/Assets/gameManger.cs
+++ My project (5)/Assets/gameManger.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class gameManger : MonoBehaviour
 {
+    int score = 0;
+    int increment = 1;
+    public Text text1;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
-}
+
+    public void increase()
+
+
+    {
+        score += increment;
+        text1.text = score.ToString();
+    }
+    
---
+++ cw5/Assets/Scripts/Paddel.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddel : MonoBehaviour
+{
+    Rigidbody2D RB;
+    public float speed;
+    float playerInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        RB = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        playerInput = Input.GetAxis("Horizontal");
+        RB.velocity = new Vector2(playerInput * speed, 0);
+    }
+}
